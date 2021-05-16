b890f5fc1aafcdaca22b3e986e49a810266fd3c1
sumukus <46552069+sumukus@users.noreply.github.com>
2021-05-17 05:02:44 +0600

    Update identifier_extract.py

--- identifier_extract.py
+++ identifier_extract.py
@@ -1,5 +1,5 @@
 
-import tokenize, json, keyword, time
+import tokenize, json, keyword
 
 def writeIdentifiersNames(repo_name, file_path_list):
 	data = {"repo_name":" ", "identifiers_name":[]}
@@ -14,7 +14,7 @@ def writeIdentifiersNames(repo_name, file_path_list):
 					for token in tokens:						
 						if token.string.isidentifier() and not keyword.iskeyword(token.string):
 							data['identifiers_name'].append(token.string)
-							#print(f"   Writing --{token.string}-- identifiers name in json file")
+							print(f"   Writing --{token.string}-- identifiers name in json file")
 				except tokenize.TokenError:
 					pass
 
@@ -32,18 +32,12 @@ def readPythonFile():
 		data = json.load(json_read_file)
 		identifiers_name_list = []
 		with open("rawIdentifiersNames.json", 'w') as json_write_file:
-			#print("Initiating the writing of Identifiers Name to json file")
+			print("Initiating the writing of Identifiers Name to json file")
 			time.sleep(2)
 			for repo_data in data:
 				repo_info = writeIdentifiersNames(repo_data['repo_name'], repo_data['python_file_name'])
 				identifiers_name_list.append(repo_info)
 			json.dump(identifiers_name_list, json_write_file, indent=4, sort_keys=True)
-		#print("Writing of Identifiers Names is successfully completed")
+		print("Writing of Identifiers Names is successfully completed")
 
-start_time = time.time()
 readPythonFile()
-duration = time.time() - start_time
-f = open("time.txt", 'w')
-f.write(str(duration))
-f.close()
-
