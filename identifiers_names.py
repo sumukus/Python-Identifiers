dbf1e3bd02b2961a416dbf3347c9dffc61f63fc5
sumukus <46552069+sumukus@users.noreply.github.com>
2021-05-17 05:07:07 +0600

    Update identifiers_names.py

--- identifiers_names.py
+++ identifiers_names.py
@@ -9,7 +9,7 @@ The structure of json file is: {"identifiers_name":[], total_count: int}
 def uniqueIdentifiers():
 	identifiers_name_list = []
 	count = 0
-	with open("atomicIdentifiersName.json", 'r') as json_read_file:
+	with open("dataset/atomicIdentifiersName.json", 'r') as json_read_file:
 		identifiers_names = {'identifiers_name':[], "total_count":0}
 		identifiers_names_set = set()
 		data = json.load(json_read_file)
